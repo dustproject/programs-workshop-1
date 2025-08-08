8b1ee8e54b543425b8d441e5ecd890e0fadabe98
vdrg <vdrg@protonmail.ch>
2025-08-08 09:55:02 -0700

    Cleanup reset script

--- script/ResetGame.s.sol
+++ script/ResetGame.s.sol
@@ -2,21 +2,13 @@
 pragma solidity >=0.8.24;
 
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
-import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 import { WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
-import { console } from "forge-std/console.sol";
-
-import { EntityId, EntityTypeLib } from "@dust/world/src/types/EntityId.sol";
-import { ProgramId } from "@dust/world/src/types/ProgramId.sol";
-import { Systems } from "@latticexyz/world/src/codegen/tables/Systems.sol";
-
-import { IWorld } from "@dust/world/src/codegen/world/IWorld.sol";
 
 import { Script } from "./Script.sol";
 
 import { Depositors } from "../src/codegen/tables/Depositors.sol";
-import { Participant, ParticipantData } from "../src/codegen/tables/Participant.sol";
+import { Participant } from "../src/codegen/tables/Participant.sol";
 
 contract ResetGame is Script {
   using WorldResourceIdInstance for ResourceId;
